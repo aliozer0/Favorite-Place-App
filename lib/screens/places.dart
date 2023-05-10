f8ca9355f8b5a09df813b43b8bd050347170ac35
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-05-10 13:44:49 +0300

    Adding Places with Provider & Displaying Places

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,14 +1,17 @@
+import 'package:favorite_place/providers/user_places.dart';
 import 'package:favorite_place/screens/add_places.dart';
 import 'package:favorite_place/widgets/place_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text(' Your Places'),
@@ -22,7 +25,9 @@ class PlacesScreen extends StatelessWidget {
               icon: Icon(Icons.add))
         ],
       ),
-      body: PlacesList(places: []),
+      body: PlacesList(
+        places: userPlaces,
+      ),
     );
   }
 }
