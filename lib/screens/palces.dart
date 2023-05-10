0343c415b4c599ed55c605f7a26498ecb893ece1
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-05-10 12:28:31 +0300

    Adding a "Places" Screen

---
+++ lib/screens/palces.dart
@@ -0,0 +1,19 @@
+import 'package:favorite_place/widgets/place_lsit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' Your Places'),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      ),
+      body: PlacesList(places: []),
+    );
+  }
+}
