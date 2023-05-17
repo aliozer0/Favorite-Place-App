89b2cef4261d6b93dd88ee09446945b54aec4876
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-05-17 12:08:53 +0300

    added

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
@@ -5,6 +6,6 @@ const uuid = Uuid();
 class Place {
   final String id;
   final String title;
-
-  Place({required this.title}) : id = uuid.v4();
+  final File image;
+  Place({required this.title, required this.image}) : id = uuid.v4();
 }
--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,15 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/place.dart';
+import 'dart:io';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title) {
-    final newPlace = Place(title: title);
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
     state = [newPlace, ...state];
   }
 }
 
-final userPlacesProvider = StateNotifierProvider<UserPlacesNotifier,List<Place>>(
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
   (ref) => UserPlacesNotifier(),
 );
